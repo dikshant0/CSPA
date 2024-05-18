947d7481962dc664af778fc02ee07184ec519dd3
Dikshant <dikshant378@gmail.com>
2024-05-18 16:44:28 +0530

    Updates

---
+++ precache-manifest.15c04629c9198aacfa5e3a0c6645b22b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe9a1a8d4ba8b84a06a27dbcd39230d",
+    "url": "/cspa/index.html"
+  },
+  {
+    "revision": "bf51031fd73871125314",
+    "url": "/cspa/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "bf51031fd73871125314",
+    "url": "/cspa/static/js/2.415eafda.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/cspa/static/js/2.415eafda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b647d9cd1f3488b1e033",
+    "url": "/cspa/static/js/main.91c5dcd6.chunk.js"
+  },
+  {
+    "revision": "4f9f90b900c15c5d4b8b",
+    "url": "/cspa/static/js/runtime-main.799cf56d.js"
+  },
+  {
+    "revision": "15324564ff20a412fd6c0f56c9b22d4c",
+    "url": "/cspa/static/media/Gender.15324564.png"
+  },
+  {
+    "revision": "71b7e1ebf5b4855978abaa0cac855b1b",
+    "url": "/cspa/static/media/Gender_Trends.71b7e1eb.png"
+  },
+  {
+    "revision": "b459fdefa1dd84cba4d37e5f0e38bff6",
+    "url": "/cspa/static/media/Geographic.b459fdef.png"
+  },
+  {
+    "revision": "7c817aa5fbbb4e22497fd5d56bc0bcab",
+    "url": "/cspa/static/media/Geographics_trend.7c817aa5.png"
+  },
+  {
+    "revision": "ab216c02341b385315438ed72aa0187e",
+    "url": "/cspa/static/media/Linear1.ab216c02.PNG"
+  },
+  {
+    "revision": "0966b676546d4b0727f017400acfb9b2",
+    "url": "/cspa/static/media/Linear2.0966b676.PNG"
+  },
+  {
+    "revision": "297f7cb6f7e32a77817cb813f39ed1fc",
+    "url": "/cspa/static/media/Logistic1.297f7cb6.png"
+  },
+  {
+    "revision": "3a250dcf08bbaa01f6f865b425a68c0e",
+    "url": "/cspa/static/media/Logistic2.3a250dcf.png"
+  },
+  {
+    "revision": "9940530b06463125f5c392baec0bede0",
+    "url": "/cspa/static/media/RandomForest.9940530b.png"
+  },
+  {
+    "revision": "3971c7b9f78bd70bf62edd185cb9afb1",
+    "url": "/cspa/static/media/TimeSeries1.3971c7b9.png"
+  },
+  {
+    "revision": "3bd8736f2155b1dfb206e5d7fbc51509",
+    "url": "/cspa/static/media/TimeSeries2.3bd8736f.png"
+  }
+]);
